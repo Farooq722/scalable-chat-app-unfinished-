a62f8843321c522903fc45b7194d8c1303da54f1
B FAROOQ <bfarooq2022@gmail.com>
2025-08-30 14:29:11 +0530

    relayer

--- user-ws/src/index.ts
+++ user-ws/src/index.ts
@@ -1,15 +1,27 @@
-import { WebSocketServer, WebSocket } from 'ws';
+import { WebSocketServer, WebSocket as WebSocketWsType } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
 interface Room {
-    sockets: WebSocket[];
+    sockets: WebSocketWsType[];
 }
 
 const rooms: Record<string, Room> = {
     // room1: {
     //     sockets: [s1, s2, s3...WebSocket.]
     // }
+};
+
+const RELAYER_URL = 'ws://localhost:3001';
+const relayerSocket = new WebSocket(RELAYER_URL);
+
+relayerSocket.onmessage = ({ data }) => {
+  // Handle incoming messages from the relayer
+  const parsedData = JSON.parse(data);
+   if(parsedData.type == "chat") {
+        const room = parsedData.room;
+        rooms[room].sockets.map(socket => socket.send(data));
+    }
 };
 
 wss.on('connection', function connection(ws) {
@@ -27,11 +39,6 @@ wss.on('connection', function connection(ws) {
             }
         }
         rooms[room].sockets.push(ws);
-    }
-
-    if(parsedData.type == "chat") {
-        const room = parsedData.room;
-        rooms[room].sockets.map(socket => socket.send(data));
     }
 });
 
